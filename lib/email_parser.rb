c8457735e1ccfac6ac2b41bd8214ab48d3b1de2b
Sam D <samidiaz3@gmail.com>
2021-03-09 03:40:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,14 +9,14 @@ class EmailAddressParser
   @@all = []
 
 
-  # def initialize (email_addresses)
-  #
-  # end
+  def initialize (emails)
+    emails.split(" ")
+  end
 
 
   def parse
     #email_addresses.split(/\ |\,/)
-    emails.split(" ")
+    #emails.split(" ")
 
   end
 
